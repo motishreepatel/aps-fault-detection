e56185627773ebbb9ebd3f770879d51c409ae7f0
motishreepatel <motishreepatel89@gmail.com>
2022-12-02 18:26:50 +0530

    Updated the requirements.txt file

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+# whenever we are working in a pythin project, the first file we need is setup.py so that whatever code or programs 
+# we are writing in our project can be used as a library in another file or project.
+# In python project, a folder which contains __init__.py file will be considered as packages
 from setuptools import find_packages,setup
 
-def 
+setup(
+    name = "sensor",
+    version = "0.0.1",
+    author = "motishree",
+    author_email = "motishreepatel89@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+
+)
